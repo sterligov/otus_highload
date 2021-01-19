c362c30177eed6895e72e6cf8b4f1a3113b09874
Sterligov Denis <denis0324@gmail.com>
2021-01-19 23:07:53 +0500

    validation

--- dating/frontend/src/components/Registration.jsx
+++ dating/frontend/src/components/Registration.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import {Link, withRouter} from "react-router-dom";
-import Dropdown from "react-dropdown";
 import axios from "axios";
+import ReactFormInputValidation from "react-form-input-validation";
 
 class Registration extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             isFormDisabled: false,
-            error: null,
+            errors: {},
             cities: [],
-            user: {
+            fields: {
                 email: "",
                 password: "",
                 first_name: "",
@@ -22,14 +22,31 @@ class Registration extends React.Component {
             }
         };
 
+        this.form = new ReactFormInputValidation(this);
+        this.form.useRules({
+            name: "required",
+            city_id: "required",
+            password: "required",
+            first_name: "required",
+            last_name: "required",
+            sex: "required",
+            interests: "required",
+            birthday: "required",
+            email: "required|email",
+        });
+
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
+
+        this.form.onformsubmit = (fields) => {
+            console.log(fields);
+        }
     }
 
     handleSubmit(e) {
+        this.form.
         e.preventDefault();
 
-        this.setState({validationErrors: {}});
+        this.setState({errors: {}});
         const user = {...this.state.user};
         console.log(user);
 
@@ -50,12 +67,12 @@ class Registration extends React.Component {
             );
     }
 
-    handleChange(e) {
-        let user = {...this.state.user};
-        const inputName = e.target.getAttribute("name");
-        user[inputName] = e.target.value;
-        this.setState({user: user, error: null});
-    }
+    // handleChange(e) {
+    //     let user = {...this.state.user};
+    //     const inputName = e.target.getAttribute("name");
+    //     fields[inputName] = e.target.value;
+    //     this.setState({user: user, error: null});
+    // }
 
     componentDidMount() {
         this.cancelSource = axios.CancelToken.source();
@@ -83,7 +100,6 @@ class Registration extends React.Component {
                     if (this._isMounted) {
                         this.setState({
                             cities: [],
-                            error: err
                         });
                     }
                 }
@@ -100,49 +116,78 @@ class Registration extends React.Component {
             <div>
                 <fieldset disabled={this.state.isFormDisabled}>
                     <form name="article" className="form-group" style={{width: "300px"}}
-                          onSubmit={this.handleSubmit}>
+                          onSubmit={this.form.handleSubmit}>
 
-                        <label htmlFor="first_name" className="mt-2 font-weight-bold">
-                            Имя
-                        </label>
+                        <label htmlFor="first_name" className="mt-2 font-weight-bold">Имя</label>
                         <input className="form-control"
                                type="text"
                                name="first_name"
-                               onChange={this.handleChange}/>
+                               onBlur={this.form.handleBlurEvent}
+                               onChange={this.form.handleChangeEvent}
+                               value={this.state.fields["first_name"]}/>
+                        <label className="error d-block text-danger">
+                            {this.state.errors["first_name"] ? this.state.errors["first_name"] : ""}
+                        </label>
 
-                        <label htmlFor="second_name" className="font-weight-bold">Фамилия</label>
+                        <label htmlFor="last_name" className="font-weight-bold">Фамилия</label>
                         <input className="form-control"
                                type="text"
-                               name="second_name"
-                               onChange={this.handleChange}/>
+                               name="last_name"
+                               onBlur={this.form.handleBlurEvent}
+                               onChange={this.form.handleChangeEvent}
+                               value={this.state.fields["last_name"]}/>
+                        <label className="error d-block text-danger">
+                            {this.state.errors["last_name"] ? this.state.errors["last_name"] : ""}
+                        </label>
 
                         <label htmlFor="email" className="mt-2 font-weight-bold">Email</label>
                         <input className="form-control"
                                type="text"
                                name="email"
-                               onChange={this.handleChange}/>
+                               onBlur={this.form.handleBlurEvent}
+                               onChange={this.form.handleChangeEvent}
+                               value={this.state.fields["email"]}/>
+                        <label className="error d-block text-danger">
+                            {this.state.errors["email"] ? this.state.errors["email"] : ""}
+                        </label>
 
                         <label htmlFor="password" className="font-weight-bold">Пароль</label>
                         <input className="form-control"
-                               type="text"
+                               type="password"
                                name="password"
-                               onChange={this.handleChange}/>
+                               onBlur={this.form.handleBlurEvent}
+                               onChange={this.form.handleChangeEvent}
+                               value={this.state.fields["password"]}/>
+                        <label className="error d-block text-danger">
+                            {this.state.errors["password"] ? this.state.errors["password"] : ""}
+                        </label>
 
                         <label htmlFor="birthday" className="font-weight-bold">Дата рождения</label>
                         <input className="form-control"
                                type="date"
                                name="birthday"
-                               onChange={this.handleChange}/>
+                               onBlur={this.form.handleBlurEvent}
+                               onChange={this.form.handleChangeEvent}
+                               value={this.state.fields["birthday"]}/>
+                        <label className="error d-block text-danger">
+                            {this.state.errors["birthday"] ? this.state.errors["birthday"] : ""}
+                        </label>
 
                         <label htmlFor="city" className="font-weight-bold">Город</label>
-                        <select name="city_id" className="form-control" onChange={this.handleChange}>
+                        <select name="city_id" className="form-control"
+                                onBlur={this.form.handleBlurEvent}
+                                onChange={this.form.handleChangeEvent}
+                                value={this.state.fields["city_id"]}>
                             {this.state.cities.map(c => {
                                 return <option value={c.id}>{c.value}</option>;
                             })}
                         </select>
 
                         <label htmlFor="sex" className="font-weight-bold">Пол</label>
-                        <select name="sex" className="form-control" onChange={this.handleChange}>
+                        <select name="sex" className="form-control"
+                                onBlur={this.form.handleBlurEvent}
+                                onChange={this.form.handleChangeEvent}
+                                value={this.state.fields["sex"]}>
                             <option value="M">Мужской</option>;
                             <option value="F">Женский</option>;
                         </select>
@@ -150,13 +195,16 @@ class Registration extends React.Component {
                         <label htmlFor="interests" className="font-weight-bold">Интересы</label>
                         <textarea className="form-control"
                                name="interests"
-                               onChange={this.handleChange}/>
+                               onBlur={this.form.handleBlurEvent}
+                               onChange={this.form.handleChangeEvent}
+                               value={this.state.fields["interests"]}/>
+                        <label className="error d-block text-danger">
+                            {this.state.errors["interests"] ? this.state.errors["interests"] : ""}
+                        </label>
 
-                        <input className="btn btn-info mt-2"
-                               type="submit"
-                               value="Зарегистрироваться"
-                               onClick={this.handleSubmit}
-                        />
+                        <button className="btn btn-info mt-2" type="submit">
+                            Зарегистрироваться
+                        </button>
 
                         <Link to="/sign-in"><button className="btn btn-info mt-2 ml-2">Назад</button></Link>
                     </form>
